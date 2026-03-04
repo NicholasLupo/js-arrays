bdf2ed6f976bbd2839866b71ace5bdfce755e90f
NicholasLupo <nicholaslupo14@gmail.com>
2026-03-04 15:59:39 +0100

    Complete bonus 2. to add the elements teachers with >= 5 characters on another array

--- arrays_1/02-arrays-bonus/script.js
+++ arrays_1/02-arrays-bonus/script.js
@@ -15,7 +15,20 @@ console.log(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+
+    let lengthTeacher = teachers[i].length;
+
+  if (lengthTeacher >= 5) {
+
+    longNames.push(teachers[i]);
+
+  }
+}
+
+console.log(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
